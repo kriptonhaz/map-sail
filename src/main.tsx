1a51b6f3b31d4566b9a6ad2452f0cc82ff0f58ea
kriptonhaz <kriptonhaz@gmail.com>
2023-12-26 20:36:58 +0700

    implement react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import { CssBaseline, ThemeProvider } from "@mui/material"
-import { BrowserRouter } from "react-router-dom"
-import { theme } from "@/themes/ts/theme"
-import "./global.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { theme } from "@/themes/ts/theme";
+import "./global.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./service/QueryClient";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
-)
+);
